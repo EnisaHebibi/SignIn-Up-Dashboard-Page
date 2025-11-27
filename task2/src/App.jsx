fbf4c87dbb9cf90ce252c86a6f3eca2a3318cc64
Enisa Hebibi <isahebibi@gmail.com>
2025-11-27 20:16:11 +0100

    setup project create Login page create Register Page

---
+++ task2/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Router</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
